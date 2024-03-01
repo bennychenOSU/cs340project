7043f457b0b7c77646a3081a9f7a0c9d78985928
Benny Chen <bechen1@berkeley.edu>
2024-02-29 16:23:36 -0800

    add query get route

--- app.js
+++ app.js
@@ -8,39 +8,23 @@ var app     = express();
 PORT        = 9124;               
 var db = require('./db-connector');
 
+// app.js
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     
+app.engine('.hbs', engine({extname: ".hbs"}));  
+app.set('view engine', '.hbs');                
+
+
 /*
     ROUTES
 */
+// app.js
+
 app.get('/', function(req, res)
     {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
-
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        let base = "<h1>MySQL Results:</h1>"
-                        res.send(base + JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+        res.render('staffings');                    
+    });                                       
 
     /*
     LISTENER
