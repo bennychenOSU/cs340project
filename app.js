7cb6245ec47d2a97fbf0b4ce89ce14854aea1260
Benny Chen <bechen1@berkeley.edu>
2024-03-04 16:40:23 -0800

    add and search

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     
 app.engine('.hbs', engine({extname: ".hbs"}));  
 app.set('view engine', '.hbs');   
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 var hbs = exphbs.create({});
 hbs.handlebars.registerHelper("staffingRowID", function(employee_id, store_id) {
@@ -31,6 +33,59 @@ hbs.handlebars.registerHelper("deleteParams", function(employee_id, store_id) {
     ROUTES
 */  
 // app.js
+
+app.get('/employees', function(req, res)
+
+    {
+        let query1;
+
+        if (req.query.search_employee_id === undefined || req.query.search_employee_id === "") {
+            query1 = "SELECT employee_id as ID, employee_name as Name, title as Title FROM Employees;"
+        } else {
+            query1 = `SELECT employee_id as ID, employee_name as Name, title as Title FROM Employees WHERE employee_id = ${req.query.search_employee_id}`;
+        }
+       
+        let query2 = `SELECT employee_id as ID FROM Employees`;
+
+    
+        db.pool.query(query1, function(error, result, fields) {
+            let employees = result;
+            
+            db.pool.query(query2, (error, result, fields) => {
+                let employee_ids = result;
+
+                   
+                res.render('employees', {data: employees, employee_ids: employee_ids});
+                
+            })
+    
+        });
+    });
+
+  app.post('/add-employee-form', function(req, res){
+
+    
+      let data = req.body;
+     
+  
+      query1 = `INSERT INTO Employees (employee_name, title) VALUES ('${data['input_name']}', '${data['input_title']}')`;
+  
+      db.pool.query(query1, function(error, rows, fields){
+  
+          if (error) {
+  
+              console.log(error)
+              res.sendStatus(400);
+          }
+  
+          else
+          {
+              res.redirect('/employees');
+          }
+      })
+  });
+  
+
 
 app.get('/', function(req, res)
 
@@ -101,13 +156,19 @@ app.post('/add-staffing-form', function(req, res){
     })
 });
 
-app.delete('/delete-staffing/', function(req,res,next){
-    console.log("here");
-    
-    let data = req.body;
-    let employee_id = parseInt(data.employee_id);
-    let store_id = parseInt(data.store_id);
+/*
+app.delete('/delete-staffing-ajax/', function(req,res,next){
 
+    let data = req.body;
+  let customerID = parseInt(data.id);
+   
+    /*
+    let data = req.body;
+    
+    let employee_id = parseInt(data.employee_id);
+    let store_id = parseInt(data.store_id); 
+
+    
     let deleteStaffing = `DELETE FROM Staffing WHERE employee_id = ? AND store_id = ?`;
   
   
@@ -123,8 +184,44 @@ app.delete('/delete-staffing/', function(req,res,next){
               else
               {
                   res.sendStatus(204);
+              } 
+  }); */
+
+  app.delete("/delete-customer-ajax/", function (req, res, next) {
+    console.log(req);
+    let data = req.body;
+    let customerID = parseInt(data.id);
+    let deleteCustomer_Cert_Customer = `DELETE FROM Customers WHERE customerID = ?`;
+    let deleteCustomer_Customer = `DELETE FROM Customers WHERE customerID = ?`;
+  /*
+    // Run the 1st query
+    db.pool.query(
+      deleteCustomer_Cert_Customer,
+      [customerID],
+      function (error, rows, fields) {
+        if (error) {
+          // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+          console.log(error);
+          res.sendStatus(400);
+        } else {
+          // Run the second query
+          db.pool.query(
+            deleteCustomer_Customer,
+            [customerID],
+            function (error, rows, fields) {
+              if (error) {
+                console.log(error);
+                res.sendStatus(400);
+              } else {
+                res.sendStatus(204);
               }
-  })});
+            }
+          );
+        }
+      }
+    ); */
+  });
+  
 
   app.put('/update-staffing', function(req,res,next){
     let data = req.body;
